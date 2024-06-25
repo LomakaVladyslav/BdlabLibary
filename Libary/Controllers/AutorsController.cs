216c8eaa751858149ba898c511f97094e9a816b9
Vladyslav <Vladyslav@VLADYSLAV>
2024-06-25 03:58:26 +0300

    Add project files.

---
+++ Libary/Controllers/AutorsController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Libary.Data;
+using Libary.Services;
+
+namespace Libary.Controllers
+{
+    public class AutorsController : Controller
+    {
+        private readonly DblibaryContext _context;
+
+        public AutorsController(DblibaryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Autors
+        public async Task<IActionResult> Index()
+        {
+            var dblibaryContext = _context.Autors.Include(a => a.Region);
+            return View(await dblibaryContext.ToListAsync());
+        }
+
+        // GET: Autors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autors
+                .Include(a => a.Region)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        // GET: Autors/Create
+        public IActionResult Create()
+        {
+            ViewData["RegionId"] = new SelectList(_context.Regions, "Id", "Id");
+            return View();
+        }
+
+        // POST: Autors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RegionId,AutorName,Pseudonym,Id")] Autor autor)
+        {
+          
+            
+                _context.Add(autor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            
+            ViewData["RegionId"] = new SelectList(_context.Regions, "Id", "Id", autor.RegionId);
+            return View(autor);
+        }
+
+        // GET: Autors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autors.FindAsync(id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            ViewData["RegionId"] = new SelectList(_context.Regions, "Id", "Id", autor.RegionId);
+            return View(autor);
+        }
+
+        // POST: Autors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RegionId,AutorName,Pseudonym,Id")] Autor autor)
+        {
+            if (id != autor.Id)
+            {
+                return NotFound();
+            }
+
+            if (true)
+            {
+                try
+                {
+                    _context.Update(autor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AutorExists(autor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RegionId"] = new SelectList(_context.Regions, "Id", "Id", autor.RegionId);
+            return View(autor);
+        }
+
+        // GET: Autors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autors
+                .Include(a => a.Region)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        // POST: Autors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var autor = await _context.Autors.FindAsync(id);
+            if (autor != null)
+            {
+                _context.Autors.Remove(autor);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AutorExists(int id)
+        {
+            return _context.Autors.Any(e => e.Id == id);
+        }
+        [HttpGet]
+        public IActionResult Import()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Import(IFormFile fileExcel, CancellationToken cancellationToken)
+        {
+            var factory = new AutorDataPortServiceFactory(_context);
+            var importService = factory.GetImportService(fileExcel.ContentType);
+
+            using var stream = fileExcel.OpenReadStream();
+            await importService.ImportFromStreamAsync(stream, cancellationToken);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Export([FromQuery] string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    CancellationToken cancellationToken = default)
+        {
+            var factory = new AutorDataPortServiceFactory(_context);
+            var exportService = factory.GetExportService(contentType);
+
+            var memoryStream = new MemoryStream();
+
+            await exportService.WriteToAsync(memoryStream, cancellationToken);
+
+            await memoryStream.FlushAsync(cancellationToken);
+            memoryStream.Position = 0;
+
+            return new FileStreamResult(memoryStream, contentType)
+            {
+                FileDownloadName = $"autors_{DateTime.UtcNow.ToShortDateString()}.xlsx"
+            };
+        }
+    }
+}
---
+++ Libary/Controllers/ChartController.cs
@@ -0,0 +1,58 @@
+﻿using Libary.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LibraryWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly DblibaryContext _context;
+
+        public ChartController(DblibaryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonData")]
+        public JsonResult JsonData()
+        {
+            var categories = _context.Publications.ToList();
+            List<object> catBook = new List<object>();
+            catBook.Add(new[] { "Категорія", "Кількість книжок" });
+            foreach (var c in categories)
+            {
+                catBook.Add(new object[] { c.BookName, c.Price });
+            }
+            return new JsonResult(catBook);
+        }
+
+        [HttpGet("PublicationAutorData")]
+        public JsonResult PublicationAutorData()
+        {
+            var publicationAutorCounts = _context.PublicationAutors
+                .GroupBy(pa => pa.Publication.BookName)
+                .Select(g => new
+                {
+                    PublicationName = g.Key,
+                    Count = g.Count()
+                })
+                .ToList();
+
+            List<object> pubAutorData = new List<object>();
+            pubAutorData.Add(new[] { "Publication", "Number of Authors" });
+            foreach (var item in publicationAutorCounts)
+            {
+                pubAutorData.Add(new object[] { item.PublicationName, item.Count });
+            }
+
+            return new JsonResult(pubAutorData);
+        }
+    }
+}
